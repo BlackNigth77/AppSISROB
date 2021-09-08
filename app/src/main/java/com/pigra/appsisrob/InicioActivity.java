bbd9ab1a9807a7fd5c129323c48acf04464173af
silvia.varillas <silvia.varillas@LAP-T490-2-03>
2021-09-07 22:26:09 -0500

    Ultima Actualizacion Silvia

--- app/src/main/java/com/pigra/appsisrob/InicioActivity.java
+++ app/src/main/java/com/pigra/appsisrob/InicioActivity.java
@@ -2,13 +2,45 @@ package com.pigra.appsisrob;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class InicioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_inicio);
+
+        //Agregar Animaciones
+
+        Animation animacion1 = AnimationUtils.loadAnimation(this,R.anim.deplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this,R.anim.desplazamiento_abajo);
+
+        TextView lblDetalle = findViewById(R.id.lblDetalle);
+        TextView lblEmpresa = findViewById(R.id.lblEmpresa);
+        ImageView imgLogoApp = findViewById(R.id.imgLogoApp);
+
+        lblDetalle.setAnimation(animacion2);
+        lblEmpresa.setAnimation(animacion2);
+        imgLogoApp.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(InicioActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
     }
 }
